830e1f9e065d9ad24eb5d06f8e09d3b171ce65ef
Ramandeep <ramandeeplohan42@gmail.com>
2026-04-21 16:30:08 +0530

    Completed logic building exercises

--- src/Dsa/Basics/Patterns/Pattern7.java
+++ src/Dsa/Basics/Patterns/Pattern7.java
@@ -8,13 +8,10 @@ public class Pattern7 {
         int n = sc.nextInt();
         for(int i=0;i<=n;i++){
             for(int j=0;j<=n-i-1;j++){
-                System.out.print(" ");
+                System.out.print("  ");
             }
-            for(int j=0;j<=(2*i)+1;j++){
-                System.out.print("*");
-            }
-            for(int j=0;j<=n-i-1;j++){
-                System.out.print(" ");
+            for(int j=1;j<=(2*i)+1;j++){
+                System.out.print("* ");
             }
             System.out.println();
         }
