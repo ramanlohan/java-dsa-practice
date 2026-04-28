573f513bfaad1e9d1a85ddfdb4f1dac1d0c15e03
Ramandeep <ramandeeplohan42@gmail.com>
2026-04-29 00:10:05 +0530

    Completed Class One.

--- src/LoveBabbarQuestions/ClassOne/Five_PrintCountingFromNTo1.java
+++ src/LoveBabbarQuestions/ClassOne/Five_PrintCountingFromNTo1.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 
 public class Five_PrintCountingFromNTo1 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n=sc.nextInt();
+        Scanner scc = new Scanner(System.in);
+        int n=scc.nextInt();
         for(int i=1;i<=n;i++){
             System.out.println(i);
         }
